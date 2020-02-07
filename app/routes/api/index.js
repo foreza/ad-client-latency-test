e95cef4e45ef08adef5d8e0a491fe1dfff0df2e0
Jason Chiu <jason.chiu@inmobi.com>
2020-02-06 17:06:41 -0800

    support-mk-ad-slot test

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,12 +1,14 @@
 
 const router = require('express').Router();
 
-
-const sessions = require('./session');            // For the restful sessions API
+const sdkView = require('./sdk');               // For serving local sdk responses
+const sessions = require('./session');          // For the restful sessions API
 const sessionView = require('./admin');         // For administrative / testing, use distort-admin path
 
+router.use('/sdk', sdkView);
 router.use('/session', sessions);
 router.use('/admin', sessionView);
+
 
 // Nobody should be accessing this.
 router.get('/', (req, res) => {
